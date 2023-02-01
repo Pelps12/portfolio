e7598ba21e7d680a32674c81ef552540f77d5325
Pelps12 <oluwapelps@gmail.com>
2023-02-01 01:24:37 -0600

    Push new changes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-#My Personal Website
+# My Personal Website
 
-##Feel free to check it out
+## Feel free to check it out
+
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/Pelps12/portfolio)
--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,21 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
-import svelte from "@astrojs/svelte";
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://oluwapelps.com',
-  integrations: [mdx(), sitemap(), tailwind(), react(), svelte()],
-  output: "server",
-  adapter: cloudflare({mode: "directory"})
+	site: 'https://oluwapelps.com',
+	integrations: [mdx(), sitemap(), tailwind(), react(), svelte()],
+	output: 'server',
+	adapter: cloudflare()
 });
